c38f3f26022e1f60eb0800b8da12999989fcf6f4
Dehestani <sdk99333@gmail.com>
2025-07-06 17:29:32 +0330

    test

--- Realm War Game/src/Game/src/main/java/org/example/Main.java
+++ Realm War Game/src/Game/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
+package Game.src.main.java.org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test>");
     }
 }
