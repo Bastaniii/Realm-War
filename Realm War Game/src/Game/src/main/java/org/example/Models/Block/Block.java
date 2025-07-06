3862592deed154a201ec04812e93dc908cdf33f1
kimiak1 <Kimiyakazempoor@gmail.com>
2025-07-06 17:49:54 +0330

    Block and EmptyBlock

--- Realm War Game/src/Game/src/main/java/org/example/Models/Block/Block.java
+++ Realm War Game/src/Game/src/main/java/org/example/Models/Block/Block.java
@@ -1,7 +1,53 @@
 package Game.src.main.java.org.example.Models.Block;
 
-public class Block {
-    int x;
 
+
+public abstract class Block {
+    protected int x;
+    protected int y;
+    protected boolean hasOwner;
+    protected int owner;
+    protected  String type;
+    protected int gold;
+    protected int food;
+    protected boolean hasBuilt;
+    public Block(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.hasOwner = false;
+        this.owner = 0;
+        this.hasBuilt = false;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public boolean hasOwner() {
+        return hasOwner;
+    }
+    public int getOwner() {
+        return owner;
+    }
+    public String getType() {
+        return type;
+    }
+    public int getGold() {
+        return gold;
+    }
+    public int getFood() {
+        return food;
+    }
+    public void changeOwner(int newOwner) {
+        hasOwner = true;
+        owner = newOwner;
+    }
+    public void build(){
+        hasBuilt = true;
+    }
+    public boolean canBuild() {
+        return !hasBuilt;
+    }
 }
 
--- Realm War Game/src/Game/src/main/java/org/example/Models/Block/EmptyBlock.java
+++ Realm War Game/src/Game/src/main/java/org/example/Models/Block/EmptyBlock.java
@@ -1,4 +1,13 @@
 package src.Game.src.main.java.org.example.Models.Block;
 
-public class EmptyBlock {
+public class EmptyBlock extends Block {
+
+    public EmptyBlock(int x, int y) {
+        super(x, y);
+        this.gold=2;
+        this.food=0;
+    }
+    public String getType() {
+        return "Empty";
+    }
 }
