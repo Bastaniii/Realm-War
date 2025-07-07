c66afa83ecef537b58e896943c8f77feb7f24daa
kimiak1 <Kimiyakazempoor@gmail.com>
2025-07-07 14:30:52 +0330

    Knight,Spearman and Swordman are completed

--- Realm War Game/src/Game/src/main/java/org/example/Models/units/Knight.java
+++ Realm War Game/src/Game/src/main/java/org/example/Models/units/Knight.java
@@ -1,4 +1,17 @@
 package Game.src.main.java.org.example.Models.units;
 
-public class Knight {
+import src.Game.src.main.java.org.example.Models.units.Unit1;
+
+public class Knight extends Unit1 {
+    public Knight() {
+        super();
+        this.hitPoint=5;
+        this.movementRange=4;
+        this.attackPower=4;
+        this.attackRange=4;
+        this.payment=5;
+        this.ration=4;
+        this.unitSpace=4;
+        this.level=4;
+    }
 }
--- Realm War Game/src/Game/src/main/java/org/example/Models/units/Spearman.java
+++ Realm War Game/src/Game/src/main/java/org/example/Models/units/Spearman.java
@@ -1,4 +1,17 @@
 package Game.src.main.java.org.example.Models.units;
 
-public class Spearman {
+import src.Game.src.main.java.org.example.Models.units.Unit1;
+
+public class Spearman extends Unit1 {
+    public Spearman(){
+        super();
+        this.hitPoint=3;
+        this.movementRange=2;
+        this.attackPower=2;
+        this.attackRange=2;
+        this.payment=3;
+        this.ration=2;
+        this.unitSpace=2;
+        this.level=2;
+    }
 }
