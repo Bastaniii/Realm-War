c67e61960b73dc0dc0ea6b96685104c281452ce2
Dehestani <sdk99333@gmail.com>
2025-07-19 15:22:48 +0330

    Structure1 and Farm1 are updated.

--- Realm War Game/src/Game/src/main/java/org/example/Models/Structures/Farm1.java
+++ Realm War Game/src/Game/src/main/java/org/example/Models/Structures/Farm1.java
@@ -1,22 +1,14 @@
 package src.Game.src.main.java.org.example.Models.Structures;
 
 public class Farm1 extends Structure1 {
-    protected int food;
-    public Farm1(int buildingCost) {
-        super(1, 3, 25, 5, buildingCost,5);
-        this.food = 5;
-        this.levelUpCost =5;
+    public Farm1(int buildingCost,int owner) {
+        super(1, 3, 50, 3, buildingCost,10,owner,"Farm",3,0,0,6,0,0);
     }
-    public int getFood(){
-        return this.food;
-    }
-    public void setFood(int food){
-        this.food = food;
-    }
-    public void upGrade (Farm1 farm){
-        farm.setFood(farm.getFood()+5);
-        farm.setLevel(farm.getLevel()+1);
-        farm.setHitPoint(farm.getHitPoint()+5);
-        farm.setLevelUpCost(farm.getLevelUpCost()+5);
+    public void upgrade () {
+        setLevel(getLevel()+1);
+        setMaintenanceCost(getMaintenanceCost()+1);
+        setLevelUpCost(getLevelUpCost()+5);
+        setFood(getFood()+5);
+        setHitPoint(getHitPoint()+25);
     }
 }
--- Realm War Game/src/Game/src/main/java/org/example/Models/Structures/Structure1.java
+++ Realm War Game/src/Game/src/main/java/org/example/Models/Structures/Structure1.java
@@ -1,28 +1,88 @@
 package src.Game.src.main.java.org.example.Models.Structures;
 
-public class Structure1 {
-    protected int level;
+public abstract class Structure1 {
+    protected static int level;
     protected int maxLevel;
     protected int hitPoint;
     protected int maintenanceCost;
-    protected int buildingCost;
-    protected int levelUpCost;
-    public Structure1(int level, int maxLevel, int hitPoint, int maintenanceCost, int buildingCost, int levelUpCost) {
+    protected static int buildingCost;
+    protected static int levelUpCost;
+    protected int owner;
+    protected String type;
+    protected int maxAmount;
+    protected int unitSpace;
+    protected int defenceRange;
+    protected int food;
+    protected int gold;
+    protected int produceNum;
+    protected boolean isPaidStructure;
+    public Structure1(int level, int maxLevel, int hitPoint, int maintenanceCost, int buildingCost, int levelUpCost,int owner,String type,int maxAmount,int unitSpace,int defenceRange,int food,int gold,int produceNum) {
         this.level = level;
         this.maxLevel = maxLevel;
         this.hitPoint = hitPoint;
         this.maintenanceCost = maintenanceCost;
         this.buildingCost = buildingCost;
         this.levelUpCost = levelUpCost;
+        this.owner = owner;
+        this.type = type;
+        this.maxAmount = maxAmount;
+        this.unitSpace = unitSpace;
+        this.defenceRange = defenceRange;
+        this.food = food;
+        this.gold = gold;
+        this.produceNum = produceNum;
+        this.isPaidStructure = false;
+    }
+    public boolean isPaidStructure() {
+        return isPaidStructure;
+    }
+    public void setPaid(boolean paid) {
+        isPaidStructure = paid;
+    }
+    public int getProduceNum (){
+        return produceNum;
+    }
+    public void setProduceNum(int produceNum) {
+        this.produceNum = produceNum;
+    }
+    public int getGold(){
+        return gold ;
+    }
+    public void setGold(int gold){
+        this.gold = gold;
+    }
+    public int getFood (){
+        return food;
+    }
+    public void setFood(int food){
+        this.food = food;
+    }
+    public int getDefenceRange(){
+        return this.defenceRange;
+    }
+    public void setDefenceRange(int defenceRange){
+        this.defenceRange = defenceRange;
+    }
+    public String getType() {
+        return type;
+    }
+    public int getUnitSpace() {
+        return unitSpace;
     }
 
-    public int getLevelUpCost() {
+    public void setUnitSpace(int unitspace) {
+        this.unitSpace = unitspace;
+    }
+    public int getowner(){
+        return owner;
+    }
+    public static int getLevelUpCost() {
         return levelUpCost;
     }
     public void setLevelUpCost(int levelUpCost) {
         this.levelUpCost = levelUpCost;
     }
-    public int getLevel() {
+    public static int getLevel() {
         return level;
     }
     public int getMaxLevel() {
@@ -34,17 +94,14 @@ public class Structure1 {
     public int getMaintenanceCost(){
         return maintenanceCost;
     }
-    public int getBuildingCost(){
+    public void setMaintenanceCost(int maintenanceCost) {
+        this.maintenanceCost = maintenanceCost;
+    }
+    public static int getBuildingCost(){
         return buildingCost;
     }
     public void setHitPoint(int hitPoint) {
         this.hitPoint = hitPoint;
-    }
-    public void setMaintenanceCost(int maintenanceCost) {
-        this.maintenanceCost = maintenanceCost;
-    }
-    public void setBuildingCost(int buildingCost) {
-        this.buildingCost = buildingCost;
     }
     public void setLevel(int level) {
         this.level = level;
@@ -59,7 +116,29 @@ public class Structure1 {
         return structure.getHitPoint() > damage;
     }
     public boolean isMaxLevel(Structure1 structure) {
-        return structure.getLevel() == maxLevel;
+        return structure.getLevel() == structure.getMaxLevel();
     }
+    public  abstract void upgrade();
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Structure1 that = (Structure1) obj;
 
+        return level == that.level &&
+                maxLevel == that.maxLevel &&
+                hitPoint == that.hitPoint &&
+                maintenanceCost == that.maintenanceCost &&
+                buildingCost == that.buildingCost &&
+                levelUpCost == that.levelUpCost &&
+                owner == that.owner &&
+                maxAmount == that.maxAmount &&
+                unitSpace == that.unitSpace &&
+                defenceRange == that.defenceRange &&
+                food == that.food &&
+                gold == that.gold &&
+                produceNum == that.produceNum &&
+                isPaidStructure == that.isPaidStructure &&
+                (type != null ? type.equals(that.type) : that.type == null);
+    }
 }
