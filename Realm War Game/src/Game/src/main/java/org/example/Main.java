900f54261ad7ce315500489ad30a3ae1afaa49c4
Dehestani <sdk99333@gmail.com>
2025-07-19 19:57:05 +0330

    Change.

--- Realm War Game/src/Game/src/main/java/org/example/Main.java
+++ Realm War Game/src/Game/src/main/java/org/example/Main.java
@@ -2,8 +2,12 @@ package src.Game.src.main.java.org.example;
 
 import src.Game.src.main.java.org.example.Controllers.GameController;
 
+import javax.swing.*;
+import java.net.URL;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         GameController gameController = new GameController();
     }
-}
+    }
